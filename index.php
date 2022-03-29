3681b5352fcf674444186fd1a317c166780f4e49
rexnard <100132176+rexnard@users.noreply.github.com>
2022-03-29 23:08:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <title>My Top 10 Favorite Movies</title>    
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    </head>
+    <body>
+        <div id= "wrapper">
+        
+        <div id="header">
+            <h1>TOP 10 FAVORITE MOVIES</h1>
+            </div>
+        
+        <div id="box-one">             
+            <h1 class="text-one">TOP 1 (NARCOS)</h1>
+             <p class="test-content-one">The true story of Colombia's infamously violent and powerful drug cartels fuels this gritty gangster drama series.</p> 
+            
+
+        </div>
+        <div id="box-two">
+            
+            <p class="test-content-two">With Spider-Man's identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his normal life as Peter Parker from the high stakes of being a superhero. When Peter asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.</p>
+            <h1 class="text-two">TOP 2 (SPIDER-MAN NO WAY HOME)</h1>
+            
+        
+        
+        
+        </div>
+        <div id="box-three"> 
+           
+            <p class="test-content-three">John is a legendary hitman who had retired until a gang invades his house, steals his car, and kills the puppy that his late wife Helen had given him.</p>
+             <h1 class="text-three">TOP 3 (JOHN WICK)</h1>
+            
+        
+        </div>
+        <div id="box-four"> 
+            
+            <p class="test-content-four">In a future where mutants are nearly extinct, an elderly and weary Logan leads a quiet life. But when Laura, a mutant child pursued by scientists, comes to him for help, he must get her to safety.</p>
+             <h1 class="text-four">TOP 4 (LOGAN)</h1>
+           
+        
+        </div>
+        <div id="box-five"> 
+           
+            <p class="test-content-five">Wolverine comes to Japan to meet an old friend whose life he saved years ago, and gets embroiled in a conspiracy involving yakuza and mutants.</p>
+             <h1 class="text-five">TOP 5 (THE WOLVERINE)</h1>
+        
+        </div>
+        <div id="box-six"> 
+            
+            <p class="test-content-six">The Avengers and the Guardians of the Galaxy attempt to prevent Thanos from collecting the six all-powerful Infinity Stones as part of his quest to kill half of all life in the universe.</p>
+            <h1 class="text-six">TOP 6 (AVENGERS INFINITY WAR)</h1>
+            
+        
+        </div>
+        <div id="box-seven"> 
+              
+            <p class="test-content-seven">The grave course of events set in motion by Thanos that wiped out half the universe and fractured the Avengers ranks compels the remaining Avengers to take one final stand in Marvel Studios' grand conclusion to twenty-two films, "Avengers: Endgame."</p>
+            <h1 class="text-seven">TOP 7 (AVENGERS ENDGAME)</h1>
+          
+        
+        </div>
+        <div id="box-eight"> 
+          
+            <p class="test-content-eight">Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to destroy. </p>
+             <h1 class="text-eight">TOP 8 (FAST AND FURIOUS)</h1>
+            
+        
+        </div>
+        <div id="box-nine"> 
+           
+            <p class="test-content-nine">Gotham City, simply known as Gotham, is a city in the United States in the County of Gotham. Notoriously corrupt and plagued by excessively high crime rates, Gotham has been dominated by a series of crime lords throughout its modern history.</p>
+            <h1 class="text-nine">TOP 9 (GOTHAM CITY)</h1>
+            
+            
+        
+        </div>
+        <div id="box-ten"> 
+            
+            <p class="test-content-ten">Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby.</p>
+            <h1 class="text-ten">TOP 10 (IRON MAN)</h1>
+           
+            
+        </div>
+            </div>
+   
+    </body>
+</hmtl>
